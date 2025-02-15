67d53109aaa74bc1b4d8293def113b5a7a294a21
Codingvibes8 <wecodeappz@gmail.com>
2025-02-15 00:13:00 +0000

    made changes to files-3*

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import dbConnect from "@/lib/dbConnect"
-import Product from "@/models/Product"
+import Product from "@/models/product"
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url)
